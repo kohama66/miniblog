21c0f9b9aef68cdc0b64e7ef7b9d651f2de369c3
kohama66 <l.my.me666666@gmail.com>
2019-09-14 21:47:58 +0900

    add editbutton

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
+    @article = Article.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
@@ -10,6 +11,9 @@ class ArticlesController < ApplicationController
   def create
     Article.create(article_params)
     redirect_to action: 'index'
+  end  
+
+  def show
   end  
 
   private
