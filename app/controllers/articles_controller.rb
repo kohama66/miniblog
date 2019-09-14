e2e5cbc8587e364669272f8fed3e624f3d805034
kohama66 <l.my.me666666@gmail.com>
2019-09-14 20:37:13 +0900

    create post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
 
   def index
-    @article = Article.all
   end
-    
+
   def new
+    @article = Article.new
   end  
+
+  def create
+    Article.create(article_params)
+    redirect_to action: 'index'
+  end  
+
+  private
+  def article_params
+    params.require(:article).permit(:content).merge(user_id: current_user.id)
+  end
 end
